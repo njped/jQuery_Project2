adf3d06a5723a8afcc1d5ed865a78753e118a339
njped <njpedigo@gmail.com>
2022-06-17 15:35:41 -0600

    finished project

---
+++ app.js
@@ -0,0 +1,93 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+storeItems.forEach(function(item){
+    if(item.inStock) {
+      $('#products').append(`<div class="row">
+      <div class="small-col">$${item.price}</div>
+      <div class="col">${item.name}</div>
+      <div class="col">${item.details}</div>
+      </div>`);
+    }
+  })
+  
+
+const container = $('body');
+const rowContainer = $('.row');
+const button = $('.clickMe');
+const borderHeader = $('h1');
+
+function darkModeToggle() {
+  container.toggleClass("darkMode");
+  rowContainer.toggleClass("darkModeRow");
+  button.toggleClass("darkModeBorder");
+  borderHeader.toggleClass("darkModeBorder");
+}
+
+button.click(darkModeToggle);
